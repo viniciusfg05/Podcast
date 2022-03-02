347cf3672861b0f81891eef5ed8b184a8a1f89da
viniciusfg05 <68232658+viniciusfg05@users.noreply.github.com>
2022-03-02 09:40:53 -0300

    Primeiro commit

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.scss'
+
+
+export function Player() {
+  return (
+    <div className={styles.PlayerContainer}>
+      <header >
+        <img src="/playing.svg" alt="Tocando agora" />
+        <strong>Tocando agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+
+      <footer>
+
+      </footer>
+    </div>
+
+  )
+}
