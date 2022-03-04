275f3c8afe6c4c35ac68e32c3be13f3ebcd5a216
vinicius <vinicius.fg05@gmail.com>
2022-03-03 22:15:47 -0300

    Formatação de dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,62 @@
+import {api} from '../services/api'
+import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
-export default function Home() {
+
+type Epispode = {
+  id: String;
+  title: String;
+  members: String
+  //....
+}
+
+type HomeProps = {
+  episodes: Epispode[];
+}
+
+//  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+
+
+export default function Home(props: HomeProps) {
+  
   return (
     <div>
-      Idex
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
+}
+
+
+export const  getStaticProps: GetStaticProps = async () => {
+  const {data} = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published-at',
+      _order: 'desc'
+    }
+  })
+  
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      published: format(parseISO(episode.published), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.duration),
+      description: episode.description,
+      url: episode.url,
+
+
+    }
+  })
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 60
+  }
 }
